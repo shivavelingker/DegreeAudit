a9e65aaa4a58f28597d33a94db4c1ce8190d8db4
Shiva Velingker <shiva@cs.utexas.edu>
2016-12-13 17:51:13 -0600

    Angular 1 Implementation: Basic page structure setup. Able to process some global data

--- main.js
+++ main.js
@@ -1,37 +1,17 @@
-window.fn = {};
-semesters = ["Before Fall 2015", "Fall 2015", "Spring 2016"];
+var semesters = ['None', 'Before Fall 2015', 'Fall 2015', 'Spring 2016', 'Fall 2017'];
 
-window.fn.open = function() {
-  var menu = document.getElementById('menu');
-  menu.open();
-};
-
-window.fn.load = function(page) {
-  var content = document.getElementById('content');
-  var menu = document.getElementById('menu');
-  content.load(page)
-    .then(menu.close.bind(menu));
-};
-
-window.fn.newCourse = function() {
-  //document.getElementById('s0').checked = true;
-  //Set up all semesters
-  var section = document.getElementById("semesters");
-  
-  var listItem = document.createElement("ons-list-item");
-  //Make tappable
-  var radioLabel = document.createElement("label");
-  //Class=left
-  listItem.appendChild(radioLabel);
-  var semesterLabel = document.createElement("label");
-  listItem.appendChild(semesterLabel);
-  //For #, Class=center
-  
-  for(var i in semesters){
-    alert(i);
-    semesterLabel.innerHtml = semesters[i];
-    alert("next");
-    section.append(listItem);
-    alert("done");
-  }
-};
+ons.bootstrap()
+  .controller('Semesters', function() {
+    this.delegate = {
+      configureItemScope: function(index, itemScope) {
+        itemScope.semester = semesters[index];
+        itemScope.index = index;
+      },
+      countItems: function() {
+        return semesters.length;
+      },
+      calculateItemHeight: function() {
+        return ons.platform.isAndroid() ? 48 : 44;
+      }
+    };
+  });
